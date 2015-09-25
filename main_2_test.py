34e009cc65896bdcff8822fa4ebf75e97b5c79ed
Lars Liverød Andersen <lars_la@live.com>
2015-09-25 11:01:48 +0200

    Added all graphs to project, now parsing node states correctly

--- main_2_test.py
+++ main_2_test.py
@@ -6,31 +6,41 @@ class Node:
         self.yPos = 0.0
 
     def __repr__(self):
-        return "ID:" + str(self.index) +" Xpos:" + str(self.xPos) + " Ypos" + str(self.yPos)
+        return "ID:" + str(self.index) +" Xpos:" + str(self.xPos) + " Ypos:" + str(self.yPos)
+
 
 class CSP:
-    def __init__(self, vars, k):
+    def __init__(self, nodes, k):
         self.k = k
-        self.vars = vars
+        self.nodes = nodes
+        self.domain = self.set_domain()
 
     def __repr__(self):
-        return "Nodes: " + str(self.vars) + "K-value: " + str(self.k)
+        return "Nodes: " + str(self.nodes) + "K-value: " + str(self.k)
 
+    def set_domain(self):
+        variable_colors = ['red', 'green', 'blue', 'yellow', 'pink', 'brown']
+        return variable_colors[0:self.k]
 
 
 def get_graph():
-    graph = open('modul2/graph.txt', 'r').read().splitlines()
+    input_graph = input("Select graph (1-6): ")
     k = int(input("K-value: "))
-    num_vertices = int(graph[0][0])
-    num_edges = graph[0][2]
+
+    input_graph = "graph" + input_graph + ".txt"
+    graph = open('modul2/' + input_graph, 'r').read().splitlines()
+
+    num_vertices = int([i for i in graph[0].split()][0])
+    num_edges = int([i for i in graph[0].split()][1])
 
     nodes = []
 
     for i in range(1, num_vertices + 1):
         node = Node(i)
-        node.index = graph[i][0]
-        node.xPos = graph[i][1:5]
-        node.yPos = graph[i][5::]
+        state = [i for i in graph[i].split()]
+        node.index = state[0]
+        node.xPos = state[1]
+        node.yPos = state[2]
         nodes.append(node)
 
     csp = CSP(nodes, k)
