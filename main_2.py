f36a281291abd4e8806a0bb9aa67e9c7a2789799
Lars Liverød <lars_la@live.com>
2015-09-28 20:40:14 +0200

    Added Constraint class, had some difficulties - will continue tomorrow. Revise has been improved. Constraint eval method has been outlined.

--- main_2.py
+++ main_2.py
@@ -1,6 +1,7 @@
-from modul2.gui import Gui
-from tkinter import *
+#from modul2.gui import Gui
+#from tkinter import *
 from algorithms.search import Problem
+from algorithms.utils import memoize
 
 class Node:
     def __init__(self, index):
@@ -14,6 +15,14 @@ class Node:
         return "ID:" + str(self.index)
 
 
+class Constraint:
+    def __init__(self, variables, method=None):
+        self.variables = variables
+        self.method = method
+        
+    def __repr__(self):
+        return "<Constraint (variables:%s)>" % self.variables
+    
 class CSP(Problem):
     def __init__(self, nodes, domain, constraints):
         self.nodes = nodes
@@ -59,7 +68,6 @@ class GAC:
                 for x in list(set(self.neighbors(var)) - set(con)):
                     self.queue.append((x, var))
 
-
     def neighbors(self, Xi):
         return ['wat']
 
@@ -69,10 +77,10 @@ class GAC:
     def revise(self, var, con):
         revised = False
         for x in var.domain:
-            # if no value y in Dj allows (x,y) to satisfy the constraint between Xi and Xj:
-            if x not in [c.domain for c in con]:
-                self.csp.prune(var, x, self.removals)
-                revised = True
+            for y in con.domain:
+                if x != y and con.method([x,y]):
+                    self.csp.prune(var, x, self.removals)
+                    revised = True
         return revised
 
     '''
@@ -86,12 +94,25 @@ class GAC:
             csp.prune(Xi, x, removals)
             revised = True
     return revised
+    
+    
+    
+    def revise(self, node, constraint):
+            new_domain = []
+            for domain_node in node.domain:
+                valid_domain = False
+                for constraint_node in constraint.vars:
+                    if constraint_node != node:
+                        for d in constraint_node.domain:
+                            if constraint.method([domain_node, d]):
+                                valid_domain = True
+                                break
     '''
 
 
 def get_graph():
     input_graph = input("Select graph (1-6): ")
-    input_graph = "graph" + input_graph + ".txt"
+    input_graph = "graph" + str(input_graph) + ".txt"
     graph = open('modul2/' + input_graph, 'r').read().splitlines()
     return graph
 
@@ -107,6 +128,9 @@ def create_nodes(num_vertices, graph):
         nodes.append(node)
     return nodes
 
+#GAC -> memoize(lambda n: n[0]!=n[1])
+#constraint = Constraint(i, GAC.method)
+
 
 def set_constraints(num_vertices, graph):
     constraints = []
@@ -117,19 +141,20 @@ def set_constraints(num_vertices, graph):
 
 
 def get_vc_domain(k):
-    variable_colors = ['red', 'green', 'blue', 'yellow', 'pink', 'brown']
+    variable_colors = ['red', 'green', 'blue', 'yellow', 'pink', 'brown', 'purple', 'orange']
     return variable_colors[0:k]
 
 
 def init_problem():
-    k = int(input("K-value: "))
+    k = int(input("K-value (3-10): "))
     graph = get_graph()
     num_vertices = int([i for i in graph[0].split()][0])
     num_edges = int([i for i in graph[0].split()][1])
 
     nodes = create_nodes(num_vertices, graph)
+    constraint = memoize(lambda n: n[0]!=n[1])
     constraints = set_constraints(num_vertices, graph)
-    vc_dom = get_vc_domain(k)
+    vc_dom = range(0,k)
     domains = {}
     for node in nodes:
         domains[node] = vc_dom
@@ -139,18 +164,79 @@ def init_problem():
     gac = GAC(csp)
     gac.initialization()
 
-    root = Tk()
-    app = Gui(csp, master=root)
-    app.mainloop()
+    #root = Tk()
+    #app = Gui(csp, master=root)
+    #app.mainloop()
 
 
 if __name__ == '__main__':
     init_problem()
 
 
+'''
+
+class Constraint():
+
+    method = None
+
+    def __init__(self):
+        self.vars = []
+        
 
 
+> [x,y,z], x + y <= z
+
+y i Dy som != x i Dx
+lambda n: return n[0] != n[1]
 
 
+def makefunc(variables, expression, envir=globals()):
+    return eval('(lambda ' + ','.join(variables)[:1] + ': ' + expression + ')', envir)
+        
+        
+        
+Constraint.method = staticmethod(makefunc(['n'], 'n[0] != n[1]'))
 
 
+def revise(self, node, constraint):
+        new_domain = []
+        for domain_node in node.domain:
+            valid_domain = False
+            for constraint_node in constraint.vars:
+                if constraint_node != node:
+                    for d in constraint_node.domain:
+                        if constraint.method([domain_node, d]):
+                            valid_domain = True
+                            break
+
+            if valid_domain:
+                new_domain.append(domain_node)
+
+        node.domain = new_domain
+        
+def zebra_constraint(A, a, B, b, recurse=0):
+        same = (a == b)
+        next_to = abs(a - b) == 1
+        if A == 'Englishman' and B == 'Red': return same
+        if A == 'Spaniard' and B == 'Dog': return same
+        if A == 'Chesterfields' and B == 'Fox': return next_to
+        if A == 'Norwegian' and B == 'Blue': return next_to
+        if A == 'Kools' and B == 'Yellow': return same
+        if A == 'Winston' and B == 'Snails': return same
+        if A == 'LuckyStrike' and B == 'OJ': return same
+        if A == 'Ukranian' and B == 'Tea': return same
+        if A == 'Japanese' and B == 'Parliaments': return same
+        if A == 'Kools' and B == 'Horse': return next_to
+        if A == 'Coffee' and B == 'Green': return same
+        if A == 'Green' and B == 'Ivory': return (a - 1) == b
+        if recurse == 0: return zebra_constraint(B, b, A, a, 1)
+        if ((A in Colors and B in Colors) or
+            (A in Pets and B in Pets) or
+            (A in Drinks and B in Drinks) or
+            (A in Countries and B in Countries) or
+            (A in Smokes and B in Smokes)): return not same        
+        raise 'error'
+
+
+        
+'''
