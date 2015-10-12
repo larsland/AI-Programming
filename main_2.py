e08c2b48abf389b2256cd22b24b1d7cb8d3ccf90
Lars Liverød Andersen <lars_la@live.com>
2015-10-12 21:48:41 +0200

    Copied content of test_2 to main_2. Deleted test_2 and test_3. Added custom scenario method in module3 gui

--- main_2.py
+++ main_2.py
@@ -1,10 +1,8 @@
-from modul2.vcgraph_problem import VertexColoringProblem
+from tkinter import Tk
+from modul2.gui import Gui
 
 if __name__ == '__main__':
-    VC = VertexColoringProblem()
-    VC.set_graph(graph='graph3.txt', dom_size=4)
-    #print(VC)
-
-
-
-
+    root = Tk()
+    app = Gui(master=root)
+    root.eval('tk::PlaceWindow %s center' % root.winfo_pathname(root.winfo_id()))
+    app.mainloop()
