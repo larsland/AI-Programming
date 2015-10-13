e073a1fc0c22338277f5a623f763b48b33e62cbd
Ivar Flakstad <ivar.flakstad@gmail.com>
2015-10-13 02:36:32 +0200

    watch me hhhuuuuuheeeeeeipp

--- algorithms/csp.py
+++ algorithms/csp.py
@@ -1,8 +1,11 @@
 from algorithms.search import Problem
 from copy import deepcopy
 from abc import abstractclassmethod
-from itertools import count
+from itertools import count, product
+
+# For debugging purposes:
 counter = count()
+DEBUG = False
 
 
 class Constraint:
@@ -16,26 +19,31 @@ class Constraint:
 
 
 class CSP(Problem):
-    def __init__(self, node_domain_map, constraints):
-        self.node_domain_map = node_domain_map
+    def __init__(self, node_domain, constraints):
+        self.node_domain = node_domain
         self.constraints = constraints
 
-        Problem.__init__(self, (node_domain_map, constraints))
+        Problem.__init__(self, (node_domain, constraints))
 
     def get_domain(self, node):
-        return self.node_domain_map[node]
+        return self.node_domain[node]
 
     def set_domain(self, node, domain):
-        self.node_domain_map[node] = domain
+        self.node_domain[node] = domain
 
     def actions(self, state):
-        # Generates children and runs the rerun method.
+        """
+        Fetches all successor nodes from a given CSP state
+        In this spesific problem that means all states with a domain
+        length greater than 1 for a random node
+        :return: The generated successor nodes
+        """
         actions = []
-        for node, dom in state.node_domain_map.items():
+        for node, dom in state.node_domain.items():
             if len(dom) > 1:
                 for j in range(len(dom)):
                     child = deepcopy(state)
-                    child.node_domain_map[node] = [dom[j]]
+                    child.node_domain[node] = [dom[j]]
                     child.rerun(node)
                     if not child.contradiction:
                         actions.append(child)
@@ -63,28 +71,29 @@ class CSP(Problem):
         pass
 
     def __repr__(self):
-        return "<CSP (n: %s, c: %s)>" % (self.node_domain_map, self.constraints)
+        return "<CSP (n: %s, c: %s)>" % (self.node_domain, self.constraints)
 
 
 class GAC:
     def __init__(self, csp):
         self.csp = csp
-        self.node_domain_map = csp.node_domain_map
+        self.node_domain = csp.node_domain
         self.constraints = csp.constraints
         self.queue = []
         self.contradiction = False
 
     def __repr__(self):
-        return "<GAC(n:%s, q:%s, c:%s)>" % (self.node_domain_map, self.queue, self.constraints)
+        return "<GAC(n:%s, q:%s, c:%s)>" % (self.node_domain, self.queue, self.constraints)
 
     def get_domain(self, node):
-        return self.node_domain_map[node]
+        return self.node_domain[node]
 
     def set_domain(self, node, domain):
-        self.node_domain_map[node] = domain
+        self.node_domain[node] = domain
 
     def initialize(self):
         self.queue = [(node, con) for node, con in self.constraints.items()]
+        print(self.queue)
 
     def domain_filtering(self):
         while self.queue:
@@ -93,7 +102,7 @@ class GAC:
             if self.revise(node, cons):
                 self.push_pairs(node, cons)
 
-    """
+    '''
     def revise_(self, node, cons):
         combinations = [self.csp.get_domain(j) for j in cons.variables if j != node]
 
@@ -108,39 +117,42 @@ class GAC:
             return False
 
         return old_len > len(self.csp.get_domain(node))
-    """
+    '''
 
     def revise(self, node, cons=None):
-        from itertools import product
         """
         Removes all inconsistent values in a domain for all possible arc from an node
         It also saves to the csp_state if the current state is a contradiction
         :param from_node: The node to run revise from
         :return: Boolean telling whether the domain was revised or not
         """
-        to_be_removed = []
-        for arc in self.constraints[node].variables:
+        removals = False
+        constraint = self.constraints[node]
+        for var in constraint.variables:
             for domain in self.get_domain(node):
                 remove = True
-                for x, y in product([domain], self.get_domain(arc)):
-                    # print('c: %s, x:%s, y:%s' % (next(counter), (arc, x), (node, y)))
-                    if self.csp.cons(x, y):
+                for x, y in product([domain], self.get_domain(var)):
+                    if DEBUG:
+                        print('c: %s, x:%s, y:%s' % (next(counter), (var, x), (node, y)))
+
+                    if constraint.method(x, y):
                         remove = False
                         break
 
                 if remove:
-                    to_be_removed.append(domain)
+                    self.prune(self.get_domain(node), domain)
+                    removals = True
 
-        for domain in to_be_removed:
-            if domain in self.get_domain(node):
-                self.get_domain(node).remove(domain)
-
-        if to_be_removed:
+        if removals:
             if not self.get_domain(node):
                 self.contradiction = True
             return True
 
         return False
+
+    def prune(self, domains, domain):
+        if domain in domains:
+            domains.remove(domain)
 
     def rerun(self, i):
         self.push_pairs(i)
--- modul3/nonograms_problem.py
+++ modul3/nonograms_problem.py
@@ -16,32 +16,34 @@ class NonogramProblem(CSP):
         Constructor for the NonogramProblem
         Will set up the grid and create all nodes with all possible permutations as domains
         """
-        self.node_domain_map = {}
+        self.node_domain = {}
         self.constraints = {}
 
         self.grid = [[]]
         self.total_cols = 0
         self.total_rows = 0
 
-        def cf(a, b):
-            r, domain_a = a
-            c, domain_b = b
-
-            return domain_a[c] == domain_b[r]
-
-        self.cons = lambda x, y: cf(x, y)
-
         self.start = NonoGACNode(self)
         self.start.initialize()
         self.start.domain_filtering()
         self.open = [self.start]
 
-        CSP.__init__(self, self.node_domain_map, self.constraints)
+        CSP.__init__(self, self.node_domain, self.constraints)
 
+    def cons(self, x, y):
+        """
+        Constraint for the Nonogram constraint satisfaction problem.
+        The contents of a cell must be the same in both row and column - otherwise it is not a valid constraint.
+        :param x: Row with both a row-unique id and its respective cells
+        :param y: Column with both a column-unique id its respective cells
+        :return: Boolean, signifying if the constraint is upheld by the input.
+        """
+        r, row = x
+        c, col = y
+        return row[c] == col[r]
 
-    def set_scenario(self, nonogram='modul3/scenarioes/sweet-one.txt'):
-
-        with open(nonogram) as f:
+    def set_scenario(self, scenario='modul3/scenarioes/0-heart.txt'):
+        with open(scenario) as f:
             cols, rows = map(int, f.readline().split())
 
             self.grid = [[False]*cols]*rows
@@ -52,32 +54,25 @@ class NonogramProblem(CSP):
             for row in range(rows):
                 r_reversed.append(list(map(int, f.readline().split())))
             for row, counts in enumerate(reversed(r_reversed)):
-                self.node_domain_map[row] = [(row, p) for p in self.gen_patterns(counts, cols)]
+                self.node_domain[row] = [(row, p) for p in self.generate_perm_patterns(counts, cols)]
 
             for col in range(cols):
                 counts = list(map(int, f.readline().split()))
-                self.node_domain_map[rows + col] = [(col, p) for p in self.gen_patterns(counts, rows)]
-
-            #for node in self.node_domain_map.keys():
-            #    print("wtf", node)
+                self.node_domain[rows + col] = [(col, p) for p in self.generate_perm_patterns(counts, rows)]
 
         if DEBUG:
             for x in range(rows + cols):
-                print(self.node_domain_map[x])
+                print(self.node_domain[x])
 
         self.constraints = {}
-        self.generate_constraints()
+        self.set_constraints()
 
         self.start = NonoGACNode(self)
         self.start.initialize()
         self.start.domain_filtering()
         self.open = [self.start]
 
-        #print('NonogramProblem initialized with %dx%d grid' % (rows, cols))
-
-
-    @staticmethod
-    def gen_patterns(counts, cols):
+    def generate_perm_patterns(self, counts, cols):
         """
         Generates pattern permutations for a given number of segments
         :param counts: A sequence of segment sizes
@@ -93,82 +88,51 @@ class NonogramProblem(CSP):
         for start in range(cols - counts[0] + 1):
             permutation = []
 
-            permutation.extend([False for _ in range(start)])
-            permutation.extend([True for _ in range(start, start + counts[0])])
+            permutation += [False for _ in range(start)]
+            permutation += [True for _ in range(start, start + counts[0])]
 
-            x = start + counts[0]
-            if x < cols:
+            sub_start = start + counts[0]
+            if sub_start < cols:
                 permutation.append(False)
-                x += 1
-            if x == cols and len(counts) == 0:
+                sub_start += 1
+
+            if sub_start == cols and len(counts) == 0:
                 permutations.append(permutation)
                 break
 
-            sub_start = x
-            sub_rows = NonogramProblem.gen_patterns(counts[1:len(counts)], cols - sub_start)
+            sub_rows = self.generate_perm_patterns(counts[1:len(counts)], cols - sub_start)
 
             for sub_row in sub_rows:
                 sub_permutation = copy.deepcopy(permutation)
-
-                sub_permutation.extend([sub_row[x - sub_start] for x in range(sub_start, cols)])
-
+                sub_permutation += [sub_row[x - sub_start] for x in range(sub_start, cols)]
                 permutations.append(sub_permutation)
 
         return permutations
 
-    def generate_constraints(self):
+    def set_constraints(self):
         """
         Generates constraint network for the problem
         In this problem it is implemented as a dictionary with a given row/col index as a key
         The respective value is a list with the index for all possible rows/columns the row/col has constraints against
         More specific this means a list of all intersecting cells between a row and a column.
         """
-
         for row in range(self.total_rows):
-            self.constraints[row] = Constraint([i for i in range(self.total_rows, self.total_rows + self.total_cols)])
-
+            self.constraints[row] = Constraint([i for i in range(self.total_rows, self.total_rows + self.total_cols)],
+                                               self.cons)
         for col in range(self.total_cols):
-            self.constraints[self.total_rows + col] = Constraint([i for i in range(0, self.total_rows)])
-
-    def get_start_node(self):
-        """
-        Returns the start node for this problem instance
-        :return: the initial state in this specific problem
-        """
-        return self.start
+            self.constraints[self.total_rows + col] = Constraint([i for i in range(0, self.total_rows)], self.cons)
 
     def is_goal(self, other):
-        return sum((len(domains) - 1) for domains in other.node_domain_map.values()) == 0
+        return sum((len(domains) - 1) for domains in other.node_domain.values()) == 0
 
     def h(self, state):
         """
-        Calculates the heuristic for a given state
-        In this problem the heuristic is calculated from the sum of all domains in the variables list
-        :param astar_state: The state to calculate h for
-        :return: The h value
+        Calculates the heuristic for a given state.
+        In this Nonogram problem the heuristic is the sum of all the domains in the variables list
+        :param state: State which the heuristic is calculated from.
+        :return: The heuristic of the state.
         """
-        h = sum((len(domains) - 1) for domains in state.node_domain_map.values())
-        return h
-
-    def actions(self, state):
-        """
-        Fetches all successor nodes from a given CSP state
-        In this spesific problem that means all states with a domain
-        length greater than 1 for a random node
-        :return: The generated successor nodes
-        """
-        # Generates children and runs the rerun method.
-        actions = []
-        for node, dom in state.node_domain_map.items():
-            if len(dom) > 1:
-                for j in range(len(dom)):
-                    child = copy.deepcopy(state)
-                    child.node_domain_map[node] = [dom[j]]
-                    child.rerun(node)
-                    if not child.contradiction:
-                        actions.append(child)
-                return actions
-        return actions
+        return sum((len(domains) - 1) for domains in state.node_domain.values())
 
     def path_cost(self, movement):
         return 1
