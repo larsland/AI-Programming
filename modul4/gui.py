44c4bfa580a72b609c99b95a536424b0bd336e10
Lars Liverød Andersen <lars_la@live.com>
2015-10-15 20:53:49 +0200

    Implemented movement with arrow-keys. Logic is very flawed, but getting there

--- modul4/gui.py
+++ modul4/gui.py
@@ -35,10 +35,15 @@ class GameWindow(Frame):
         self.tiles = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
         self.board = [[0 for x in range(4)] for x in range(4)]
 
+        self.master.bind("<Up>", self.move_up)
+        self.master.bind("<Down>", self.move_down)
+        self.master.bind("<Left>", self.move_left)
+        self.master.bind("<Right>", self.move_right)
+
         self.grid_cells = []
         self.init_grid()
         self.update_view()
-    #end
+
 
     def init_grid(self):
         background = Frame(self, bg = BACKGROUND_COLOR_GAME, width = SIZE, height = SIZE )
@@ -50,11 +55,7 @@ class GameWindow(Frame):
 
             for j in range(GRID_LEN):
                 # Loop columns
-                cell = Frame(
-                            background,
-                            bg     = BACKGROUND_COLOR_CELL_EMPTY,
-                            width  = SIZE/GRID_LEN,
-                            height = SIZE/GRID_LEN)
+                cell = Frame(background, bg = BACKGROUND_COLOR_CELL_EMPTY, width = SIZE/GRID_LEN, height = SIZE/GRID_LEN)
 
                 cell.grid(row=i, column=j, padx=GRID_PADDING, pady=GRID_PADDING)
                 t = Label(master=cell, text="", bg=BACKGROUND_COLOR_CELL_EMPTY, justify=CENTER, font=FONT, width=4, height=2)
@@ -64,10 +65,7 @@ class GameWindow(Frame):
             self.grid_cells.append(grid_row)
 
         self.add_init_tiles()
-        print(self.grid_cells)
-        print(self.board)
-        self.play()
-    #end
+
 
     def update_view(self):
         print("UPDATING")
@@ -88,97 +86,123 @@ class GameWindow(Frame):
                                                 fg   = foreground_color )
         self.update_idletasks()
 
-    def move(self, direction):
-        if direction == 'up':
-            print("Moving UP")
-            for i in range(3):
-                for j in range(3):
-                    tile = self.board[i][j]
-                    print(tile)
-                    print("BOARD BEFORE: ", self.board)
-                    if tile != 0:
-                        self.board[i+1][j] = tile
-                        self.board[i][j] = 0
-                    print("BOARD AFTER: ", self.board)
+    def move_up(self, event):
+        print("MOVED UP")
+        for i in range(0, 4):
+            for j in range(0, 4):
+                tile = self.board[i][j]
+                if tile != 0:
+                    if i-1 <= -1:
+                        pass
+                    else:
+                        if self.board[i-1][j] == tile:
+                            self.board[i-1][j] += 1
+                        elif self.board[i-1][j] == 0:
+                            self.board[i-1][j] = tile
+                        else:
+                            pass
 
-        elif direction == 'down':
-            print("Moving DOWN")
-            for i in range(3):
-                for j in range(3):
-                    tile = self.board[i][j]
-                    print(tile)
-                    print("BOARD BEFORE: ", self.board)
-                    if tile != 0:
-                        self.board[i-1][j] = tile
-                        self.board[i][j] = 0
-                    print("BOARD AFTER: ", self.board)
+                        if i+1 >= 4:
+                            self.board[i][j] = 0
+                        else:
+                            self.board[i][j] = self.board[i+1][j]
+        self.next_step()
 
-        elif direction == 'left':
-            print("Moving LEFT")
-            for i in range(3):
-                for j in range(3):
-                    tile = self.board[i][j]
-                    print(tile)
-                    print("BOARD BEFORE: ", self.board)
-                    if tile != 0:
-                        self.board[i][j-1] = tile
-                        self.board[i][j] = 0
-                    print("BOARD AFTER: ", self.board)
+    def move_down(self, event):
+        print("MOVED DOWN")
+        for i in range(0, 4):
+            for j in range(0, 4):
+                tile = self.board[i][j]
+                if tile != 0:
+                    if i+1 >= 4:
+                        pass
+                    else:
+                        if self.board[i+1][j] == tile:
+                            self.board[i+1][j] += 1
+                        elif self.board[i+1][j] == 0:
+                            self.board[i+1][j] = tile
+                        else:
+                            pass
 
-        elif direction == 'right':
-            print("Moving RIGHT")
-            for i in range(3):
-                for j in range(3):
-                    tile = self.board[i][j]
-                    print(tile)
-                    print("BOARD BEFORE: ", self.board)
-                    if tile != 0:
-                        self.board[i][j+1] = tile
-                        self.board[i][j] = 0
-                    print("BOARD AFTER: ", self.board)
+                        if i-1 <= -1:
+                            self.board[i][j] = 0
+                        else:
+                            self.board[i][j] = self.board[i-1][j]
+        self.next_step()
+
+    def move_left(self, event):
+        print("MOVED LEFT")
+        for i in range(0, 4):
+            for j in range(0, 4):
+                tile = self.board[i][j]
+                if tile != 0:
+                    if j-1 <= -1:
+                        pass
+                    else:
+                        if self.board[i][j-1] == tile:
+                            self.board[i][j-1] += 1
+                        elif self.board[i][j-1] == 0:
+                            self.board[i][j-1] = tile
+                        else:
+                            pass
+
+                        if j+1 >= 4:
+                            self.board[i][j] = 0
+                        else:
+                            self.board[i][j] = self.board[i][j+1]
+        self.next_step()
+
+    def move_right(self, event):
+        print("MOVED RIGHT")
+        for i in range(0, 4):
+            for j in range(0, 4):
+                tile = self.board[i][j]
+                if tile != 0:
+                    if j+1 >= 4:
+                        pass
+                    else:
+                        if self.board[i][j+1] == tile:
+                            self.board[i][j+1] += 1
+                        elif self.board[i][j+1] == 0:
+                            self.board[i][j+1] = tile
+                        else:
+                            pass
+
+                        if j-1 <= -1:
+                            self.board[i][j] = 0
+                        else:
+                            self.board[i][j] = self.board[i][j-1]
+        self.next_step()
+
+    def next_step(self):
         self.update_view()
-
-
-
-
-
-
-
-    def play(self):
-        while True:
-            key = input("Key: ")
-            if key == 'q':
-                break
-            elif key == '\x1b[A':
-                self.move('up')
-            elif key == '\x1b[B':
-                self.move('down')
-            elif key == '\x1b[D':
-                self.move('left')
-            elif key == '\x1b[C':
-                self.move('right')
-
-            if self.game_won():
-                print("Congratulations, you have reached a tile with 2048")
-
-            if self.game_lost():
-                print("Game over")
-
-    def game_won(self):
-        return False
-
-    def game_lost(self):
-        return False
+        self.add_random_tile()
 
     def add_init_tiles(self):
-        tile_1 = random.sample(range(0, 3), 2)
-        tile_2 = random.sample(range(0, 3), 2)
+        tile_1 = random.sample(range(0, 4), 2)
+        tile_2 = random.sample(range(0, 4), 2)
 
         self.board[tile_1[0]][tile_1[1]] = 1
         self.board[tile_2[0]][tile_2[1]] = 1
 
         self.update_view()
 
+    def add_random_tile(self):
+        empty_spots = []
+        for i in range(0, 4):
+            for j in range(0, 4):
+                if self.board[i][j] == 0:
+                    empty_spots.append((i, j))
+
+        tile = random.choice(empty_spots)
+
+        self.board[tile[0]][tile[1]] = 1
+
+    def game_won(self):
+        return False
+
+    def game_lost(self):
+        return False
 
 if __name__ == '__main__':
     root = Tk()
