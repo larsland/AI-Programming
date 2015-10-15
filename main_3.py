90be21b143ee95db2b88d3501888cc485152ba5b
Lars Liverød Andersen <lars_la@live.com>
2015-10-15 15:22:18 +0200

    Initial GUI

--- main_3.py
+++ main_3.py
@@ -4,4 +4,5 @@ from modul3.gui import Gui
 if __name__ == '__main__':
     root = Tk()
     app = Gui(master=root)
+    # root.eval('tk::PlaceWindow %s center' % root.winfo_pathname(root.winfo_id()))
     app.mainloop()
