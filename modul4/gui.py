dc0324923b97aea54d7e6aceaeebc8cda6196a0d
Ivar Flakstad <ivar.flakstad@gmail.com>
2015-10-16 13:13:37 +0200

    better colors, supports huge numbers in terms of coloring. Distribution of random tiles implemented, they are 2:90%, and 4:10%.

--- modul4/gui.py
+++ modul4/gui.py
@@ -1,38 +1,36 @@
 import random
 from tkinter import *
+from tkinter import font
 
-GRID_LEN              = 4
-GRID_PADDING          = 10
-SIZE                  = 500
+GRID_LEN = 4
+GRID_PADDING = 10
+SIZE = 500
 
-BACKGROUND_COLOR_GAME       = "#92877d"
+BACKGROUND_COLOR_GAME = "#92877d"
 BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
-FONT                        = ("Verdana", 40, "bold")
-BACKGROUND_COLOR_DICT       = {
-                                1  : '#eee4da',
-                                2  : '#ede0c8',
-                                3  : '#f2b179',
-                                4  : '#f59563',
-                                5  : '#f67c5f',
-                                6  : '#f65e3b',
-                                7  : '#edcf72',
-                                8  : '#edcc61',
-                                9  : '#edc850',
-                                10 : '#edc53f',
-                                11 : '#FFAE00',
-                                12 : '#FFAE00',
-                                13 : '#FFAE00',
-                                14 : '#FFAE00',
-                                15 : '#FFAE00',
-                                16 : '#FFAE00'
-                            }
+BACKGROUND_COLOR_CELL_FALLBACK = "#404040"
+BACKGROUND_COLOR_DICT = {
+    1: '#E9DED0',
+    2: '#E8DAB9',
+    3: '#EAA35D',
+    4: '#EC8348',
+    5: '#EC6B47',
+    6: '#EB4925',
+    7: '#E6C84F',
+    8: '#E6C53A',
+    9: '#edc850',
+    10: '#edc53f',
+    11: '#FFAE00'
+}
+
 
 class GameWindow(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
+        self.font = font.Font(master, family="Verdana", size=40, weight="bold")
         self.master.title('2048')
         self.grid()
-        self.tiles = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.tiles = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.board = [[0 for x in range(4)] for x in range(4)]
 
         self.master.bind("<Up>", self.move_up)
@@ -44,9 +42,8 @@ class GameWindow(Frame):
         self.init_grid()
         self.update_view()
 
-
     def init_grid(self):
-        background = Frame(self, bg = BACKGROUND_COLOR_GAME, width = SIZE, height = SIZE )
+        background = Frame(self, bg=BACKGROUND_COLOR_GAME, width=SIZE, height=SIZE)
         background.grid()
 
         for i in range(GRID_LEN):
@@ -55,10 +52,11 @@ class GameWindow(Frame):
 
             for j in range(GRID_LEN):
                 # Loop columns
-                cell = Frame(background, bg = BACKGROUND_COLOR_CELL_EMPTY, width = SIZE/GRID_LEN, height = SIZE/GRID_LEN)
+                cell = Frame(background, bg=BACKGROUND_COLOR_CELL_EMPTY, width=SIZE / GRID_LEN, height=SIZE / GRID_LEN)
 
                 cell.grid(row=i, column=j, padx=GRID_PADDING, pady=GRID_PADDING)
-                t = Label(master=cell, text="", bg=BACKGROUND_COLOR_CELL_EMPTY, justify=CENTER, font=FONT, width=4, height=2)
+                t = Label(master=cell, text="", bg=BACKGROUND_COLOR_CELL_EMPTY, justify=CENTER, font=self.font, width=4,
+                          height=2)
                 t.grid()
                 grid_row.append(t)
 
@@ -69,21 +67,21 @@ class GameWindow(Frame):
 
     def update_view(self):
         print("UPDATING")
-        for i in range( GRID_LEN ):
-            for j in range( GRID_LEN ):
+        for i in range(GRID_LEN):
+            for j in range(GRID_LEN):
                 digit = self.board[i][j]
                 if digit == 0:
                     self.grid_cells[i][j].configure(
-                                                text = "",
-                                                bg   = BACKGROUND_COLOR_CELL_EMPTY)
+                        text="",
+                        bg=BACKGROUND_COLOR_CELL_EMPTY)
                 else:
-                    foreground_color = '#f9f6f2' if digit > 2 else '#776e65'
-                    number = 1 << digit # the human friendly representation
+                    foreground_color = '#F7F4EE' if digit > 2 else '#635B51'
+                    number = 1 << digit  # the human friendly representation
 
                     self.grid_cells[i][j].configure(
-                                                text = str( 1 << digit ),
-                                                bg   = BACKGROUND_COLOR_DICT[ digit ],
-                                                fg   = foreground_color )
+                        text=str(1 << digit),
+                        bg=BACKGROUND_COLOR_DICT.get(digit, BACKGROUND_COLOR_CELL_FALLBACK),
+                        fg=foreground_color)
         self.update_idletasks()
 
     def move_up(self, event):
@@ -93,21 +91,21 @@ class GameWindow(Frame):
             for j in range(0, 4):
                 tile = self.board[i][j]
                 if tile != 0:
-                    if i-1 <= -1:
+                    if i - 1 <= -1:
                         pass
                     else:
-                        if self.board[i-1][j] == tile:
-                            self.board[i-1][j] += 1
-                        elif self.board[i-1][j] == 0:
-                            self.board[i-1][j] = tile
+                        if self.board[i - 1][j] == tile:
+                            self.board[i - 1][j] += 1
+                        elif self.board[i - 1][j] == 0:
+                            self.board[i - 1][j] = tile
                         else:
                             moved = False
 
                         if moved:
-                            if i+1 >= 4:
+                            if i + 1 >= 4:
                                 self.board[i][j] = 0
                             else:
-                                self.board[i][j] = self.board[i+1][j]
+                                self.board[i][j] = self.board[i + 1][j]
                         else:
                             pass
         self.next_step()
@@ -119,21 +117,21 @@ class GameWindow(Frame):
             for j in range(0, 4):
                 tile = self.board[i][j]
                 if tile != 0:
-                    if i+1 >= 4:
+                    if i + 1 >= 4:
                         pass
                     else:
-                        if self.board[i+1][j] == tile:
-                            self.board[i+1][j] += 1
-                        elif self.board[i+1][j] == 0:
-                            self.board[i+1][j] = tile
+                        if self.board[i + 1][j] == tile:
+                            self.board[i + 1][j] += 1
+                        elif self.board[i + 1][j] == 0:
+                            self.board[i + 1][j] = tile
                         else:
                             moved = False
 
                         if moved:
-                            if i-1 <= -1:
+                            if i - 1 <= -1:
                                 self.board[i][j] = 0
                             else:
-                                self.board[i][j] = self.board[i-1][j]
+                                self.board[i][j] = self.board[i - 1][j]
                         else:
                             pass
         self.next_step()
@@ -145,21 +143,21 @@ class GameWindow(Frame):
             for j in range(0, 4):
                 tile = self.board[i][j]
                 if tile != 0:
-                    if j-1 <= -1:
+                    if j - 1 <= -1:
                         pass
                     else:
-                        if self.board[i][j-1] == tile:
-                            self.board[i][j-1] += 1
-                        elif self.board[i][j-1] == 0:
-                            self.board[i][j-1] = tile
+                        if self.board[i][j - 1] == tile:
+                            self.board[i][j - 1] += 1
+                        elif self.board[i][j - 1] == 0:
+                            self.board[i][j - 1] = tile
                         else:
                             moved = False
 
                         if moved:
-                            if j+1 >= 4:
+                            if j + 1 >= 4:
                                 self.board[i][j] = 0
                             else:
-                                self.board[i][j] = self.board[i][j+1]
+                                self.board[i][j] = self.board[i][j + 1]
                         else:
                             pass
         self.next_step()
@@ -171,21 +169,21 @@ class GameWindow(Frame):
             for j in range(0, 4):
                 tile = self.board[i][j]
                 if tile != 0:
-                    if j+1 >= 4:
+                    if j + 1 >= 4:
                         pass
                     else:
-                        if self.board[i][j+1] == tile:
-                            self.board[i][j+1] += 1
-                        elif self.board[i][j+1] == 0:
-                            self.board[i][j+1] = tile
+                        if self.board[i][j + 1] == tile:
+                            self.board[i][j + 1] += 1
+                        elif self.board[i][j + 1] == 0:
+                            self.board[i][j + 1] = tile
                         else:
                             moved = False
 
                         if moved:
-                            if j-1 <= -1:
+                            if j - 1 <= -1:
                                 self.board[i][j] = 0
                             else:
-                                self.board[i][j] = self.board[i][j-1]
+                                self.board[i][j] = self.board[i][j - 1]
                         else:
                             pass
         self.next_step()
@@ -209,16 +207,17 @@ class GameWindow(Frame):
             for j in range(0, 4):
                 if self.board[i][j] == 0:
                     empty_spots.append((i, j))
-
-        tile = random.choice(empty_spots)
-
-        self.board[tile[0]][tile[1]] = 1
+        if empty_spots:
+            tile = random.choice(empty_spots)
+            n = 1 if random.randint(0, 100) < 90 else 2
+            self.board[tile[0]][tile[1]] = n
 
     def game_won(self):
         return False
 
     def game_lost(self):
         return False
+
 
 if __name__ == '__main__':
     root = Tk()
