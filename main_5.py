759a85932f94a6ec773acb7e071fc41b69d9395a
lars <lars_la@live.com>
2015-11-15 15:23:24 +0100

    save

--- main_5.py
+++ main_5.py
@@ -19,13 +19,13 @@ class ImageRecognizer:
         self.hidden_nodes = hidden_nodes
         self.hidden_layers = hidden_layers
 
-        self.build_ann()
+        self.build_network()
 
     # Setting default Theano bit width
     def floatX(self, X):
         return np.asarray(X, dtype=theano.config.floatX)
 
-    def build_ann(self):
+    def build_network(self):
         input = T.fmatrix()
         target = T.fmatrix()
 
@@ -35,21 +35,20 @@ class ImageRecognizer:
         weights.append(theano.shared(np.random.uniform(low=-.1, high=.1, size=(self.hidden_nodes[1], self.hidden_nodes[2]))))
         weights.append(theano.shared(np.random.uniform(low=-.1, high=.1, size=(self.hidden_nodes[2], 10))))
 
-        activation_1 = Tann.softplus(T.dot(input, weights[0]))
-        activation_2 = Tann.softplus(T.dot(activation_1, weights[1]))
-        activation_3 = Tann.softplus(T.dot(activation_2, weights[2]))
-        activation_4 = Tann.softmax(T.dot(activation_3, weights[3]))
+        input_layer = Tann.softplus(T.dot(input, weights[0]))
+        hidden_layer1 = Tann.softplus(T.dot(input_layer, weights[1]))
+        hidden_layer2 = Tann.sigmoid(T.dot(hidden_layer1, weights[2]))
+        output_layer = Tann.softplus(T.dot(hidden_layer2, weights[3]))
 
-        error = T.sum(pow((target - activation_4), 2))
+        error = T.sum(pow((target - output_layer), 2))
         params = [weights[0], weights[1], weights[2], weights[3]]
         gradients = T.grad(error, params)
 
-        self.get_x1 = theano.function(inputs=[input, target], outputs=error, allow_input_downcast=True)
-        self.train = theano.function(inputs=[input, target], outputs=error, updates=self.RMSprop(params, gradients),
+        self.train = theano.function(inputs=[input, target], outputs=error, updates=self.rms_prop(params, gradients),
                                      allow_input_downcast=True)
-        self.predict = theano.function(inputs=[input], outputs=activation_4, allow_input_downcast=True)
+        self.predict = theano.function(inputs=[input], outputs=output_layer, allow_input_downcast=True)
 
-    def RMSprop(self, params, gradients, rho=0.9, epsilon=1e-6):
+    def rms_prop(self, params, gradients, rho=0.9, epsilon=1e-6):
         updates = []
         for p, g in zip(params, gradients):
             acc = theano.shared(p.get_value() * 0.)
@@ -60,7 +59,7 @@ class ImageRecognizer:
             updates.append((p, p - self.lrate * g))
         return updates
 
-    def do_training(self, epochs=1, test_interval=None, errors=[]):
+    def train_network(self, epochs=1, test_interval=None, errors=[]):
         starttime = time()
         for i in range(epochs):
             print("epoch: ", i)
@@ -84,7 +83,7 @@ class ImageRecognizer:
         print("Training time: " + str((time()-starttime)) + " sec")
         return errors
 
-    def do_testing(self, nr_of_testing_images=10000, scatter=True, blind_test_images=None):
+    def test_network(self, nr_of_testing_images=10000, scatter=True, blind_test_images=None):
         if blind_test_images is not None:
             self.test_images = blind_test_images
             self.test_labels = None
@@ -105,8 +104,8 @@ class ImageRecognizer:
 
     def blind_test(self, images):
         #Raw images is a list with sublist of raw_images
-        self.preprosessing(images)
-        raw_results = self.do_testing(blind_test_images=images)
+        self.preprosess_images(images)
+        raw_results = self.test_network(blind_test_images=images)
         results = []
         for i in range(len(raw_results)):
             highest_value = int(np.where(raw_results[i] == max(raw_results[i]))[0][0])
@@ -114,7 +113,7 @@ class ImageRecognizer:
         #Returns a list with the classifications of the images
         return results
 
-    def preprosessing(self, feature_sets):
+    def preprosess_images(self, feature_sets):
         # Scales images to have values between 0.0 and 1.0 instead of 0 and 255
         for image in range(len(feature_sets)):
             for value in range(len(feature_sets[image])):
@@ -130,17 +129,17 @@ class ImageRecognizer:
 
 if __name__ == '__main__':
     # input nodes, hidden nodes, learning rate, batch size, hidden layers
-    image_recog = ImageRecognizer([20, 100, 30], 0.01, 50, 3)
+    image_recog = ImageRecognizer([20, 100, 100], 0.01, 50, 3)
 
-    image_recog.preprosessing(image_recog.images)
-    image_recog.preprosessing(image_recog.test_images)
+    image_recog.preprosess_images(image_recog.images)
+    image_recog.preprosess_images(image_recog.test_images)
 
     errors = []
     start_time = time()
 
-    errors = image_recog.do_training(epochs=20, errors=errors)
+    errors = image_recog.train_network(epochs=10, errors=errors)
 
-    test_labels, result = image_recog.do_testing(nr_of_testing_images=nr_of_testing_images)
+    test_labels, result = image_recog.test_network(nr_of_testing_images=nr_of_testing_images)
     print("Total time elapsed: " + str((time() - start_time)/60) + " min")
 
 
