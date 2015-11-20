c795b23aa01fb60022a2d469cda7bb7009543058
lars <lars_la@live.com>
2015-11-20 20:21:38 +0100

    Cleaned up, rewrote the testing method, merged it with check_result method

--- modul5/ann.py
+++ modul5/ann.py
@@ -91,27 +91,17 @@ class ANN:
         print("Training time: " + str('%.2f' % (time() - start_time) + " sec"))
         return errors
 
-    def test_network(self):
+    def test_on_testing_images(self):
         labels = []
+        count = 0
         for i in range(len(self.test_images)):
             label = self.predict([self.test_images[i]])
             labels.append(label)
-        self.check_result(labels)
-
-        '''
-        hidden_activations = []
-        i = 0
-        j = self.batch_size
-        while j < len(self.test_images):
-            image_group = self.test_images[i:j]
-            i += self.batch_size
-            j += self.batch_size
-            end = self.predict(image_group)
-            for res in end:
-                hidden_activations.append(res)
-
-        self.check_result(hidden_activations)
-        '''
+        for i in range(len(labels)):
+            b = int(self.test_labels[i]) == np.argmax(labels[i])
+            if b:
+                count += 1
+        print("Correct classification:", '%.5f' % ((count/float(len(self.test_labels))) * 100))
 
     def blind_test(self, feature_sets):
         feature_sets = scale_images(feature_sets)
@@ -122,29 +112,19 @@ class ANN:
             labels.append(label)
         return labels
 
-    def check_result(self, result):
-        count = 0
-        for i in range(len(result)):
-            b = int(self.test_labels[i]) == np.argmax(result[i])
-            if b:
-                count += 1
-        print("Correct classification:", '%.5f' % ((count/float(len(self.test_labels))) * 100))
-
     def run(self):
         self.images = scale_images(self.images)
         self.test_images = scale_images(self.test_images)
-
         errors = []
 
         while True:
             print('-'*30 + '\n' + '1: Train' + '\n' + '2: Test' + '\n' +
                   '3: Blind Test' + '\n' + '4: Exit' + '\n' + '-'*30)
-
             key_input = int(input("Input: "))
             if key_input == 1:
                 errors = self.train_network(errors)
             elif key_input == 2:
-                self.test_network()
+                self.test_on_testing_images()
             elif key_input == 3:
                 minor_demo(self)
             elif key_input == 4:
