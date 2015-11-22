66dcc97b2d3d5f5bd29db602fcd1ee7c8c171c22
Ivar Flakstad <ivar.flakstad@gmail.com>
2015-11-22 17:38:03 +0100

    2048-data achieved

--- main_6.py
+++ main_6.py
@@ -3,28 +3,50 @@ import theano.tensor.nnet as Tann
 from modul5.basics.mnist_basics import *
 
 from modul5.utils import rectify
+import copy
+from modul5.ann import *
+import math
+import numpy as np
+
+
+def read_2048_file(file_path):
+    data = []
+    with open('modul6/mini.txt') as mini:
+        block = {}
+        block['board'] = []
+        for line in mini:
+            if line.find('Move') == 0:
+                if block != {'board': []}:
+                    block['board'] = np.array(block['board']).flatten()
+                    yield block
+                    block = {}
+                block['board'] = []
+                block['score'] = line.split('=')[1].rstrip('\r\n')
+            else:
+                # print(line)
+                row = line.split(' ')
+                flat_board = []
+                if len(row) > 1:
+                    for item in row:
+                        item = item.rstrip('\r\n')
+                        if item != '':
+                            item = int(item)
+                            if item == 0:
+                                flat_board.append(0)
+                            else:
+                                flat_board.append(int(math.log2(int(item))))
+                    block['board'].append(flat_board)
+                else:
+                    item = row[0].rstrip('\r\n')
+                    if item != '':
+                        block['move'] = int(item)
+
+        block['board'] = np.array(block['board']).flatten()
+        yield block
+
 
 if __name__ == '__main__':
-    '''
-    Arguments for each configuration:
-    1: hidden nodes in each hidden layer
-    2: activation functions in each layer
-    3: learning rate
-    4: batch size
-    5: number of hidden layers
-    6: number of epochs
-    7: error function for backpropagation
-    '''
 
-    cases = gen_flat_cases(60000)
-    test_cases = gen_flat_cases(10000, type="testing")
-
-    #ann1 = ANN(cases, test_cases, [170], [Tann.softplus, Tann.softplus, Tann.softmax], 0.002, 50, 1, 50, 'sum')
-    ann2 = ANN(cases, test_cases, [784], [Tann.softplus, Tann.softplus, Tann.softmax], 0.001, 100, 1, 10, 'mean')
-    #ann3 = ANN(cases, test_cases, [50, 50], [rectify, rectify, rectify, Tann.softmax], 0.01, 100, 2, 5, 'sum')
-    #ann4 = ANN(cases, test_cases, [200], [rectify, Tann.sigmoid, Tann.sigmoid], 0.001, 50, 1, 5, 'sum')
-    #ann5 = ANN(cases, test_cases, [30, 40, 50], [Tann.softplus, Tann.softplus, Tann.softplus, Tann.softplus, Tann.softplus], 0.01, 50, 3, 10, 'sum')
-
-    ann2.run()
-
-
+    #ann2 = ANN(cases, test_cases, [535], [Tann.softplus, Tann.softplus, Tann.softmax], 0.001, 100, 1, 10, 'mean')
+    for block in read_2048_file('modul6/mini.txt'):
+        print(block)
